ea0fff40a6f69dc110ac3271b8f09d41e6f7d383
Romy Chu III <romy.chu-iii@ocsbstudent.ca>
2019-04-25 07:33:58 -0400

    Well Then...

---
+++ 30.StringEqualityRChuIII/30.StringEqualityRChuIII/StringEqualityForm.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace _30.StringEqualityRChuIII
+{
+    partial class frmStringEquality
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblWhatToDo = new System.Windows.Forms.Label();
+            this.lblString1 = new System.Windows.Forms.Label();
+            this.lblString2 = new System.Windows.Forms.Label();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.txtString1 = new System.Windows.Forms.TextBox();
+            this.txtString2 = new System.Windows.Forms.TextBox();
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblWhatToDo
+            // 
+            this.lblWhatToDo.AutoSize = true;
+            this.lblWhatToDo.Location = new System.Drawing.Point(12, 9);
+            this.lblWhatToDo.Name = "lblWhatToDo";
+            this.lblWhatToDo.Size = new System.Drawing.Size(337, 13);
+            this.lblWhatToDo.TabIndex = 0;
+            this.lblWhatToDo.Text = "Enter two strings to check if they are the same (Case does not matter).";
+            // 
+            // lblString1
+            // 
+            this.lblString1.AutoSize = true;
+            this.lblString1.Location = new System.Drawing.Point(12, 38);
+            this.lblString1.Name = "lblString1";
+            this.lblString1.Size = new System.Drawing.Size(52, 13);
+            this.lblString1.TabIndex = 1;
+            this.lblString1.Text = "String 1 : ";
+            // 
+            // lblString2
+            // 
+            this.lblString2.AutoSize = true;
+            this.lblString2.Location = new System.Drawing.Point(12, 64);
+            this.lblString2.Name = "lblString2";
+            this.lblString2.Size = new System.Drawing.Size(52, 13);
+            this.lblString2.TabIndex = 2;
+            this.lblString2.Text = "String 2 : ";
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.AutoSize = true;
+            this.lblOutput.Location = new System.Drawing.Point(12, 100);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(89, 13);
+            this.lblOutput.TabIndex = 3;
+            this.lblOutput.Text = "The Strings Are : ";
+            // 
+            // txtString1
+            // 
+            this.txtString1.Location = new System.Drawing.Point(70, 35);
+            this.txtString1.Name = "txtString1";
+            this.txtString1.Size = new System.Drawing.Size(123, 20);
+            this.txtString1.TabIndex = 4;
+            // 
+            // txtString2
+            // 
+            this.txtString2.Location = new System.Drawing.Point(70, 61);
+            this.txtString2.Name = "txtString2";
+            this.txtString2.Size = new System.Drawing.Size(123, 20);
+            this.txtString2.TabIndex = 5;
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Location = new System.Drawing.Point(274, 58);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(75, 23);
+            this.btnCheck.TabIndex = 6;
+            this.btnCheck.Text = "Check";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.btnCheck_Click);
+            // 
+            // frmStringEquality
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(403, 149);
+            this.Controls.Add(this.btnCheck);
+            this.Controls.Add(this.txtString2);
+            this.Controls.Add(this.txtString1);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.lblString2);
+            this.Controls.Add(this.lblString1);
+            this.Controls.Add(this.lblWhatToDo);
+            this.Name = "frmStringEquality";
+            this.Text = "String Equality Program By Romy I. Chu III";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblWhatToDo;
+        private System.Windows.Forms.Label lblString1;
+        private System.Windows.Forms.Label lblString2;
+        private System.Windows.Forms.Label lblOutput;
+        private System.Windows.Forms.TextBox txtString1;
+        private System.Windows.Forms.TextBox txtString2;
+        private System.Windows.Forms.Button btnCheck;
+    }
+}
+
---
+++ 30.StringEqualityRChuIII/30.StringEqualityRChuIII/StringEqualityForm.cs
@@ -0,0 +1,78 @@
+﻿/*
+ * Created by: Romy I. Chu III
+ * Created on: 17-04-2019
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #34 - String Equality
+ * This program... Compares two strings to see if they're the same.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _30.StringEqualityRChuIII
+{
+    public partial class frmStringEquality : Form
+    {
+        public frmStringEquality()
+        {
+            InitializeComponent();
+        }
+
+        private bool StringsAreEqual(string string1, string string2)
+        {
+            //Declare variable.
+            bool isEqual;
+
+            //Changes the strings to all upper case.
+            string1 = string1.ToUpper();
+            string2 = string2.ToUpper();
+
+            //Checks if the strings are equal
+            if (string1 == string2)
+            {
+                //Set the bool to true
+                isEqual = true;
+            }
+            else
+            {
+                //Set the bool to false
+                isEqual = false;
+            }
+
+            //Retrun the bool
+            return isEqual;
+
+        }
+
+        private void btnCheck_Click(object sender, EventArgs e)
+        {
+            //Decalring variables
+            string string1, string2;
+            bool isEqual;
+
+            //Getting the strings from the textboxes
+            string1 = txtString1.Text;
+            string2 = txtString2.Text;
+
+            //Calls the function StringsAreEqual to check if the strings are equal.
+            isEqual = StringsAreEqual(string1, string2);
+
+            //Shows the answer.
+            if (isEqual == true)
+            {
+                lblOutput.Text = "The Strings Are : Equal";
+            }
+            else
+            {
+                lblOutput.Text = "The Strings Are : Not Equal";
+            }
+        }
+    }
+}
